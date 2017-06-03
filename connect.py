afb6e5ab83497839b74f988bcd78826eaf867052
Andres Root <info@andresroot.co>
2017-06-03 15:21:40 -0500

    working on api

--- connect.py
+++ connect.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 import core
 
 app = Flask(__name__)
@@ -16,15 +16,19 @@ def client_token():
 
 @app.route("/checkout", methods=["POST"])
 def create_purchase():
-    # nonce_from_the_client = request.form["payment_method_nonce"]
+    nonce_from_the_client = request.form['payment_method_nonce']
+    amount = request.form['amount']
+    print(amount)
     result = core.braintree.Transaction.sale({
-        'amount': '10.00',
-        'payment_method_nonce': 'fake-valid-nonce',
+        'amount': amount,
+        # 'payment_method_nonce': 'fake-valid-nonce',
+        'payment_method_nonce': nonce_from_the_client,
         'options': {
             'submit_for_settlement': True
         }
     })
-    return result
+    # import ipdb; ipdb.set_trace()
+    return result.transaction.Status.Authorized
 
 
 if __name__ == "__main__":
